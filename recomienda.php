984d6eaaf5a5ad74e952e2b119e443003a31b343
Drago Colja <dcolja@gmail.com>
2016-02-05 15:02:38 +0100

    varios

---
+++ recomienda.php
@@ -0,0 +1,110 @@
+<?php include "header.php"; ?>
+<div class="row space-up">
+	<div class="col-md-3"><img src="http://www.elforodeltenis.com/Themes/BeCool_2/images/elforodeltenis.gif" alt="el foro del tenis"></div>
+	<div class="col-md-6"></div>
+	<div class="col-md-3">
+		<a href="http://www.elforodeltenis.com">
+			<h1>Volver al foro...</h1>
+		</a>
+	</div>
+</div>
+<div class="row space-up">
+	<div class="col-md-3">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="http://www.elforodeltenis.com/addons/banners/img/recomienda.jpg" alt="Recomienda el foro del tenis" class="img-thumbnail">
+			</div>
+		</div>
+	</div>
+	<div class="col-md-9">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Consigue gratis la camiseta oficial de elforodeltenis.com</h3>
+					</div>
+					<div class="panel-body">
+						<h4>Estimado <?php echo $member_name;?>, recomi&eacute;ndanos entre 10 de tus amigos, <strong>y si consigues que al menos 5 de ellos se registran en nuestro foro</strong>, te enviaremos gratuitamente la camiseta oficial a tu casa o trabajo.</h4>
+					</div>
+				</div>
+			</div>	
+			<div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Tu estado:</h3>
+					</div>
+					<div class="panel-body">
+						<table class="table table-condensed table-bordered">
+							<tbody>
+								<tr>
+									<td>Recomendados:</td>
+									<td>0/10</td>
+								</tr>
+								<tr>
+									<td>Registrados:</td>
+									<td>0/5</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="alert alert-info">
+					<p>Ya puedes comenzar a recomendarnos, introduce los emails de tus amigos, para que les enviemos una invitación a registrarse en el foro. Cada vez que uno de ellos se registre te avisaremos. Siempre podr&aacute;s comprobar aqu&iacute; adem&aacute;s los que se hayan registrado.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h3 class="panel-title">Comenzar a recomendar:</h3>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...1"></div>
+						<div class="col-md-2"></div>
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...6"></div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row space-up">
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...2"></div>
+						<div class="col-md-2"></div>
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...7"></div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row space-up">
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...3"></div>
+						<div class="col-md-2"></div>
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...8"></div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row space-up">
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...4"></div>
+						<div class="col-md-2"></div>
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...9"></div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row space-up">
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...5"></div>
+						<div class="col-md-2"></div>
+						<div class="col-md-4"><input type="text" class="form-control" name="email1" placeholder="email amigo...10"></div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row space-up">
+						<div class="col-md-12">
+							<div style="float:right;"><button class="btn btn-success">Enviar invitaciones</button></div>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
+
+
+<?php include "footer.php"; ?>
