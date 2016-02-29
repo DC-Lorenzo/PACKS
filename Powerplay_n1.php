0208a4fd3db4e353f619dcfd3d8dd279ebcbb33f
DC-Lorenzo <dcolja@gmail.com>
2016-02-29 11:25:54 +0100

    aa

--- Powerplay_n1.php
+++ Powerplay_n1.php
@@ -49,7 +49,7 @@ $mail->Send();
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en">
+<html lang="es">
   <head>
   <!--GOOGLE ANALYTICS-->
 <script>
